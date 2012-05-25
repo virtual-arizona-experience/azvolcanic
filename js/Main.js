d3c1bd132236075e14ae281d722ceb1074a04a66
Ryan Clark <ryan.clark.j@gmail.com>
2012-05-25 17:29:16 -0500

    Remove ESRI Layers

--- js/Main.js
+++ js/Main.js
@@ -6,7 +6,14 @@ function init(){
 		historicLayer = new L.TileLayer(historicUrl, {maxZoom: 10}); 
 	
 	/* ESRI tiled service example: */
-	var natGeoLayer = new L.TileLayer.ESRI("http://services.arcgisonline.com/ArcGIS/rest/services/NatGeo_World_Map/MapServer");
+	//var natGeoLayer = new L.TileLayer.ESRI("http://services.arcgisonline.com/ArcGIS/rest/services/NatGeo_World_Map/MapServer");
+	
+	// Cloudmade / OpenStreetMap tiled layer
+	var cmUrl = 'http://{s}.tile.cloudmade.com/f7d28795be6846849741b30c3e4db9a9/997/256/{z}/{x}/{y}.png',
+		cmAttribution = 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
+		cmOptions = { maxZoom: 18, attribution: cmAttribution };
+	
+	var cloudmade = new L.TileLayer(cmUrl, cmOptions);//, {styleId: 999});
 	
 	/* Bing maps example: 
 	var bingLayer = new L.TileLayer.Bing(<<Bing Maps API Key>>, "Road"); */
@@ -55,5 +62,5 @@ function init(){
 	}); 
 	
 	var center = new L.LatLng(34.1618, -111.53332);
-	map.setView(center, 7).addLayer(natGeoLayer).addLayer(wfsLayer);
+	map.setView(center, 7).addLayer(cloudmade).addLayer(wfsLayer);
 }
